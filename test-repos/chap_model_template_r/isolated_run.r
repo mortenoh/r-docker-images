2ccbac36215d20ff0155fd1f8d5721988564ee53
Morten Hansen <morten@winterop.com>
2026-01-06 14:40:25 +0100

    refactor: convert test-repos to git submodules

--- test-repos/chap_model_template_r/isolated_run.r
+++
@@ -1,5 +0,0 @@
-source("train.r")
-source("predict.r")
-
-train_chap("input/trainData.csv", "output/model.bin")
-predict_chap("output/model.bin", "input/trainData.csv", "input/futureClimateData.csv", "output/predictions.csv")
